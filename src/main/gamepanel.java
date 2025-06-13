8dc4f7be7defbe357b90ee63325b64c29b51937f
manhanbanhcanh <daemonns128@gmail.com>
2025-06-13 21:30:46 +0700

    nothing changed, just trying out git

--- src/main/gamepanel.java
+++ src/main/gamepanel.java
@@ -1,4 +1,26 @@
 package main;
 
-public class gamepanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class gamepanel extends JPanel {
+
+    //screen settings
+    final int originalTileSize = 16; //16x16 tile
+    final int scaledTileSize = 4;
+
+    final int tileSize = originalTileSize * scaledTileSize; // scaled tile size (64px)
+    final int maxTilesColumns = 14;
+    final int maxTilesRows = 23;
+    final int screenWidth = tileSize * maxTilesRows; // 1472px
+    final int screenHeight = tileSize * maxTilesColumns; // 896px
+    // idk why i thought 23:14 ratio screen would be optimal but yes
+
+    public gamepanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true); // drawing from this panel done in an offscreen painting buffer (better rendering performance)
+    }
+
 }
